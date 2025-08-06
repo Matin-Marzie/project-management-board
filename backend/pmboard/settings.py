5c7dc9e2b66272dbc6925bbd06941cb124c6a0bf
Matin <matin.marzie5148@gmail.com>
2025-08-06 14:38:06 +0300

    Add Docker-compose, create docker container for mysql, connect to databse

--- backend/pmboard/settings.py
+++ backend/pmboard/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,8 +79,12 @@ WSGI_APPLICATION = 'pmboard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pmboard',
+        'USER': 'pmboard',
+        'PASSWORD': '1234',
+        'HOST': 'pmboard-backend-Mysql',  # your MySQL container name
+        'PORT': '3306',                   # mapped port on host
     }
 }
 
--- database/pmboard.sql
+++ database/pmboard.sql
@@ -1,3 +1,5 @@
+USE pmboard;
+
 -- phpMyAdmin SQL Dump
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
